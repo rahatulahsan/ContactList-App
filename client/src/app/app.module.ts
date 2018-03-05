526b35f101fa5aa22469e6df10787bb2c55277cb
unknown <rahatulahsan@gmail.com>
2018-03-05 16:23:48 +0600

    Adding Contact Component Html and total app done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 
@@ -12,7 +12,9 @@ import { ContactsComponent } from './contacts/contacts.component';
     ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -15,6 +15,38 @@ export class ContactsComponent implements OnInit {
   phone: string;
   constructor(private contactService: ContactService) { }
 
+
+  addContact() {
+    const newContact = {
+      first_name: this.first_name,
+      last_name: this.last_name,
+      phone: this.phone
+    };
+    this.contactService.addContact(newContact)
+    .subscribe(contact => {
+      this.contacts.push(contact);
+      this.contactService.getContacts()
+      .subscribe( contacts =>
+      this.contacts = contacts);
+    });
+  }
+
+
+  deleteContact(id: any) {
+    const contacts = this.contacts;
+    this.contactService.deleteContact(id)
+    .subscribe(data => {
+      // tslint:disable-next-line:triple-equals
+      if (data.n == 1) {
+        for (let i = 0; i < contacts.length; i++) {
+          // tslint:disable-next-line:triple-equals
+          if (contacts[i]._id == id) {
+            contacts.splice(i, 1);
+          }
+        }
+      }
+    });
+  }
   ngOnInit() {
     this.contactService.getContacts()
     .subscribe( contacts =>
