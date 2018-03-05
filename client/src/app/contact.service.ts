4b7afce6f58a653c435415e7f6af2e72b6f6b6fb
unknown <rahatulahsan@gmail.com>
2018-03-05 14:54:02 +0600

    Added Contact Schema and Business Logic

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Contact } from './contact';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+
+
+  // Retieving contactService
+  getContacts()
+  // tslint:disable-next-line:one-line
+  {
+    return this.http.get('http://localhost:3000/api/contacts')
+    .map(res => res.json());
+  }
+
+  // add contact method
+  addContact(newContact) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/api/contact', newContact, {headers: headers})
+    .map(res => res.json());
+  }
+
+  // delete method
+  deleteContact(id) {
+    return this.http.delete('http://localhost:3000/api/contact/' + id)
+    .map(res => res.json());
+  }
 }
--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ContactService } from '../contact.service';
+import { Contact } from '../contact';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  contacts: Contact[];
+  contact: Contact;
+  first_name: string;
+  last_name: string;
+  phone: string;
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+    this.contactService.getContacts()
+    .subscribe( contacts =>
+    this.contacts = contacts);
   }
 
 }
